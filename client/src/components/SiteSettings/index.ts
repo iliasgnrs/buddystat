968d3b875226bc5c7af43893447d1f2d074a67ce
Ilias Giannaros <iliasgnrs@gmail.com>
2026-02-11 19:53:02 +0200

    Export EmailReportsManager from SiteSettings index

--- client/src/components/SiteSettings/index.ts
+++ client/src/components/SiteSettings/index.ts
@@ -1,3 +1,4 @@
 export { SiteSettings } from "./SiteSettings";
 export { ScriptBuilder } from "./ScriptBuilder";
 export { SiteConfiguration } from "./SiteConfiguration";
+export { EmailReportsManager } from "./EmailReportsManager";
